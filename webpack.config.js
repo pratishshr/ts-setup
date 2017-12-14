52ca4260d6657455446041aea2c83cd084a330c9
pratishshr <vanroshr@gmail.com>
2017-12-14 18:17:29 +0545

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'lib.js',
+    library: 'lib',
+    libraryTarget: 'umd'
+  }
+};
